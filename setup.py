a6a9f56152030860ae64ac38e7fde37b74cb86f2
Saket Shukla <saket@assystant.com>
2024-06-28 10:05:19 +0530

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="dynamic-form",
-    version="0.0.3",
+    version="0.0.4",
     author="Assystant Technologies Pvt Ltd",
     description="",
     install_requires=[
